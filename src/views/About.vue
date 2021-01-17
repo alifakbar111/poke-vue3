42e0875bfce4bef535318a4af1c044bbf92789dc
alifakbar111 <alifakbar111@gmail.com>
2021-01-17 14:43:52 +0700

    fix: error on class

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,18 @@
 <template>
-  <div
-    v-if="pokemon"
-    className="w-3/12 m-auto bg-purple-50 mt-4 p-4 shadow-2xl flex justify-center flex-col items-center"
-  >
-    <h3 className="text-2xl text-green-900 uppercase">{{ pokemon.name }}</h3>
-    <div class="flex justify-center">
-      <img className="w-48" :src="pokemon.sprites.front_shiny" alt="" />
-      <img className="w-48" :src="pokemon.sprites.back_shiny" alt="" />
-    </div>
-    <h3 class="text-yellow-400">Types</h3>
-    <div v-for="(type, idx) in pokemon.types" :key="idx">
-      <h5 class="text-blue-900">{{ type.type.name }}</h5>
+  <div class="container mx-auto">
+    <div
+      v-if="pokemon"
+      class="w-3/12 m-auto bg-purple-50 mt-4 p-4 shadow-2xl flex justify-center flex-col items-center"
+    >
+      <h3 class="text-2xl text-green-900 uppercase">{{ pokemon.name }}</h3>
+      <div class="flex justify-center">
+        <img class="w-48" :src="pokemon.sprites.front_shiny" alt="" />
+        <img class="w-48" :src="pokemon.sprites.back_shiny" alt="" />
+      </div>
+      <h3 class="text-yellow-400">Types</h3>
+      <div v-for="(type, idx) in pokemon.types" :key="idx">
+        <h5 class="text-blue-900">{{ type.type.name }}</h5>
+      </div>
     </div>
   </div>
 </template>
@@ -20,14 +22,13 @@ import { reactive, toRefs } from "vue";
 export default {
   setup() {
     const route = useRoute();
-    const pokemon = reactive({ pokemon: null });
+    const state = reactive({ pokemon: null });
     fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}/`)
       .then(res => res.json())
       .then(data => {
-        pokemon.pokemon = data;
-        console.log("pokemon data", data);
+        state.pokemon = data;
       });
-    return { ...toRefs(pokemon) };
+    return { ...toRefs(state) };
   }
 };
 </script>
